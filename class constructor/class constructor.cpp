ee59bfa456939f8d48e14d9bde0371378c97c355
janemir <144587149+janemir@users.noreply.github.com>
2024-01-29 17:03:14 +0300

    Добавьте файлы проекта.

---
+++ class constructor/class constructor.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream> 
+using namespace std;
+
+class Point {
+private:
+    int x;
+    int y;
+
+public: 
+    Point(int valueX, int valueY) { // constructor
+        x = valueX;
+        y = valueY;
+    }
+    int  GetX() { // method receiving the x
+        return x;
+    }
+    void SetX(int valueX) { // method that establishes the x
+        x = valueX;
+    }
+    int  GetY() { // method receiving the y
+        return y;
+    }
+    void SetY(int valueY) { // method that establishes the y
+        y = valueY * 2;
+    }
+    void Print() { // method for displaying the values of x and y
+        cout << "X = " << x << "\t Y = " << y << endl << endl;
+    }
+};
+
+int main() {
+    Point a(5, 44);
+    a.Print();
+
+    Point b(10, 28);
+    b.Print();
+
+    return 0;
+}
+
+
